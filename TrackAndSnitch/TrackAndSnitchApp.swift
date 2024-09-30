ee2f68ef5739b8d4691a77a899bd4eaac55de3ac
ShathaMK <shathamukhaild@gmail.com>
2024-09-30 10:20:07 +0300

    Initial Commit

---
+++ TrackAndSnitch/TrackAndSnitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackAndSnitchApp.swift
+//  TrackAndSnitch
+//
+//  Created by Shatha Almukhaild on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct TrackAndSnitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
